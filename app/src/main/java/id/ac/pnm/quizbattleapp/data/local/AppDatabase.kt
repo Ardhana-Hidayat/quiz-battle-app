357bac1a76af633a992cd7c924d56aa6cd801fda
rahmaxyn <kunnisofarahma06@gmail.com>
2026-06-02 13:43:12 +0700

    Fix: added entity and DAO, Version 2

--- app/src/main/java/id/ac/pnm/quizbattleapp/data/local/AppDatabase.kt
+++ app/src/main/java/id/ac/pnm/quizbattleapp/data/local/AppDatabase.kt
@@ -3,10 +3,16 @@ package id.ac.pnm.quizbattleapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import id.ac.pnm.quizbattleapp.data.model.GameHistory
 import id.ac.pnm.quizbattleapp.data.model.Question
 
-@Database(entities = [Question::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Question::class, GameHistory::class],
+    version = 2,
+    exportSchema = false
+)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun questionDao(): QuestionDao
+    abstract fun gameHistoryDao(): GameHistoryDao
 }
